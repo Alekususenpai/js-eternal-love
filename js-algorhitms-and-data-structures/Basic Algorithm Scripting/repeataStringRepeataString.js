02f747ebf23f3840cba68c31240ee51537ed7e19
Aleksandra Jovanovska <aa_lara@live.com>
2023-02-10 15:11:14 +0100

    repeat a string task

---
+++ js-algorhitms-and-data-structures/Basic Algorithm Scripting/repeataStringRepeataString.js
@@ -0,0 +1,16 @@
+/**
+ * Repeat a String Repeat a String
+
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+ */
+
+function repeatStringNumTimes(str, num) {
+    let concat = '';
+    while (num > 0) {
+        num--;
+        concat += str;
+    }
+    return concat;
+}
+
+repeatStringNumTimes("abc", 4);

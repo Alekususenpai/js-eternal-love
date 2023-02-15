f89101f040815a86a36c8ac507ea31e292cdf9ed
Aleksandra Jovanovska <aleksandra.jovanovska@deptagency.com>
2023-02-15 15:32:41 +0100

    mutations

---
+++ js-algorhitms-and-data-structures/Regular Expressions/mutations.js
@@ -0,0 +1,40 @@
+/**
+ * Mutations
+ * 
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+
+ */
+
+function mutation(arr) {
+    const yesno = [];
+    for (let i = 0; i < arr[1].length; i++) {
+        if (arr[0].toLowerCase().includes(arr[1][i].toLowerCase())) {
+            yesno.push(true)
+        } else {
+            yesno.push(false)
+        }
+    }
+    return yesno.every(el =>
+        el === true)
+}
+
+mutation(["hello", "Hello"]);
+
+
+// or 
+
+function mutation(arr) {
+    const test = arr[1].toLowerCase();
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+}

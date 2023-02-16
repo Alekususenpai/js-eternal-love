4deeeace50333794e05dc0c1b721fcc26e26825a
Aleksandra Jovanovska <aleksandra.jovanovska@deptagency.com>
2023-02-16 11:15:26 +0100

    findersKeepers

---
+++ js-algorhitms-and-data-structures/Basic Algorithm Scripting/findersKeepers.js
@@ -0,0 +1,15 @@
+/**
+ * Finders Keepers
+ * 
+Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+ */
+
+function findElement(arr, func) {
+    const result = arr.find(el => {
+        return func(el);
+    });
+
+    return result;
+}
+
+findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; });
